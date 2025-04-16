f1e8eeda8828151dc8c56612c8e5b03c36d7f6df
Gildas <herbreteau.gildas@hotmail.fr>
2025-04-16 16:28:08 +0200

    Add user statistics endpoint and integrate Chart.js and Dropzone for enhanced user data visualization and file uploads

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -9,7 +9,6 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,6 +18,7 @@ use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\UX\Dropzone\Form\DropzoneType;
 
 class UserType extends AbstractType
 {
@@ -106,7 +106,7 @@ class UserType extends AbstractType
         }
 
         $builder
-            ->add('photo', FileType::class, [
+            ->add('photo', DropzoneType::class, [
                 'label' => 'Ma photo :',
                 'mapped' => false,
                 'required' => false,
